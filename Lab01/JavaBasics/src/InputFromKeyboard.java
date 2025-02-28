909357d2a516229240b547377a0e67d1dc90912c
QuocKhanhLuong <khanhlq.hust.work@gmail.com>
2025-02-28 21:37:35 +0700

    Exercises 6.1  and 6.2

---
+++ Lab01/JavaBasics/src/InputFromKeyboard.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class InputFromKeyboard {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("What's your name?");
+        String strName = keyboard.nextLine();
+        System.out.print("How old are you?");
+        int iAge = keyboard.nextInt();
+        System.out.print("How tall are you (m)?");
+        double dHeight = keyboard.nextDouble();
+
+        //similar to other data types
+        //nextByte(), nextShort(), nextLong()
+        //nextFloat(), nextBoolean()
+
+        System.out.print("Mrs/Ms. " + strName + ", " + iAge + " years old. " + "Your height is " + dHeight + ".");
+    }
+}
